46dbe16b16efc706615501804a48404edff9d53e
jgdml <47838646+jgdml@users.noreply.github.com>
2019-11-08 12:54:07 -0300

    Add files via upload

---
+++ instalarBibliotecas.bat
@@ -0,0 +1,15 @@
+@echo off
+
+call npm install react-native-reanimated
+
+call npm install react-native-gesture-handler
+
+call npm install react-native-screens@^1.0.0-alpha.23
+
+call npm install react-native-router-flux
+
+call npm install react-native-vector-icons
+
+call npm install react-native-elements
+
+call react-native link
