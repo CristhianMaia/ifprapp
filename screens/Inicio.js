1683d9289400fcfcc30ba6b1cf725a3009b59023
jgdml <jgdml2801@gmail.com>
2019-11-08 14:04:00 -0300

    Inicio

--- screens/Inicio.js
+++ screens/Inicio.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, StyleSheet, Image } from "react-native"
 import { Button } from "react-native-elements"
 import { Actions } from "react-native-router-flux"
 
@@ -7,13 +7,58 @@ export default class extends React.Component{
 
     render(){
         return(
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, alignItems: "center"}}>
+
+                <Image 
+                source={require("../assets/logo.png")}
+                style={styles.img} 
+                resizeMode="contain"/>
 
                 <Button 
-                title="teste"
+                title="Cadastro"
+                type="outline"
+                titleStyle={{color: "green"}}
+                buttonStyle={{borderColor: "green"}}
+                raised={true}
+                containerStyle={styles.botao}
                 onPress={() => Actions.cadastro()}
                 />
+
+                <Button 
+                title="Esqueci a senha"
+                type="outline"
+                titleStyle={{color: "green"}}
+                buttonStyle={{borderColor: "green"}}
+                raised={true}
+                containerStyle={styles.botao}
+                onPress={() => null}
+                />
+
+                <Button 
+                title="Suporte"
+                type="outline"
+                titleStyle={{color: "green"}}
+                buttonStyle={{borderColor: "green"}}
+                raised={true}
+                containerStyle={styles.botao}
+                onPress={() => null}
+                />
+
             </View>
         )
     }
 }
+
+
+
+const styles = StyleSheet.create({
+    botao: {
+        width: "50%",
+        backgroundColor: "green",
+        marginTop: 10
+    },
+    img: {
+        width: "100%",
+        height: "15%"
+    }
+})
