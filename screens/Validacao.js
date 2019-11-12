d6d805a5a72c99ad2c50fec3e4292adaa3b17131
Cristhian <cristhian.smaia@gmail.com>
2019-11-12 13:35:16 -0300

    Tooltip no campo RA e correção de states.

--- screens/Validacao.js
+++ screens/Validacao.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { View, Alert, Text } from "react-native"
+import { View, Alert, Text, TouchableHighlight } from "react-native"
 import { Input, Icon } from "react-native-elements"
 import { MaskService } from 'react-native-masked-text'
+import Tooltip from 'react-native-walkthrough-tooltip';
 import { Actions } from "react-native-router-flux"
 import Styles from "../components/Styles"
 import Button from "../components/Button"
@@ -11,7 +12,9 @@ export default class Validacao extends React.Component{
     constructor(props){
         super(props)
         this.state = {
+            resp: "",
             cpf: "",
+            ra: "",
             dtnasc: "",
             //Dados ficticios de como os dados chegarão na state após consulta ao banco via api
             responsaveis: [
@@ -24,7 +27,7 @@ export default class Validacao extends React.Component{
                     "nome": "Ciclano Responsavel 1"
                 }
             ],
-            ra: [
+            registro: [
                 {
                     "id": "2019000112",
                     "nome": "Cristhian da Silveira Maia"
@@ -36,42 +39,46 @@ export default class Validacao extends React.Component{
             ]
         }
     }
-    resp = ""
-    ra = ""
 
 
-    checkEmpty = () => {
-        if (this.resp && this.state.cpf && this.ra && this.state.dtnasc){
-            Actions.cadastro()
-        }
-        else{
-            Alert.alert("Erro", "Preencha todos os campos.")
-        }
+    submitForm = () => {
+        this.checkEmpty()
+        //Comparar dados 
+        //--------------
     }
 
+    //Mascara Campo CPF
     handleCpf = (value) => {
         var cpf = MaskService.toMask('cpf', value, {})
         this.setState({cpf: cpf})
 
     }
-
-    handleDate = (value) => {
+    //Marcara Campo Data de Nascimento
+    handleDtnasc = (value) => {
         var data = MaskService.toMask('datetime', value, {
             format: 'DD/MM/YYYY'
         })
-        this.setState({data: data})
+        this.setState({dtnasc: data})
     }
     
+    checkEmpty = () => {
+        if (this.state.resp && this.state.cpf && this.state.ra && this.state.dtnasc) {
+            Actions.cadastro()
+        }
+        else {
+            Alert.alert("Erro", "Preencha todos os campos.")
+        }
+    }
+
     render(){
         return(
             <View style={Styles.container}>
-                <Text>{this.state.data}</Text>
                 <Input 
                     label="Nome do Responsável"
                     inputContainerStyle={Styles.input}
                     labelStyle={{color: "green"}}
                     maxLength={60}
-                    onChangeText={(txt) => this.resp = txt}
+                    onChangeText={(value) => this.setState({ resp: value })}
                 />
 
                 <Input 
@@ -90,13 +97,25 @@ export default class Validacao extends React.Component{
                     keyboardType="numeric"
                     inputContainerStyle={Styles.input}
                     labelStyle={{color: "green"}}
-                    onChangeText={(txt) => this.ra = txt}
+                    onChangeText={(value) => this.setState({ ra: value })}
                     maxLength={15}
                     rightIcon={
-                        <Icon 
-                        name="questioncircleo"
-                        type="antdesign"
-                        color="green"/>
+                        <Tooltip
+                            isVisible={this.state.toolTipVisible}
+                            content={
+                                <Text>O RA (Numero de Matricula) pode ser encontrado no sistema academico 
+                                    SIGAA (sigaa.ifpr.edu.br) ou na Secretária do seu campus.</Text>
+                                }
+                            placement="left"
+                            onClose={() => this.setState({ toolTipVisible: false })}
+                        >
+                            <Icon
+                                onPress={() => this.setState({ toolTipVisible: true })}
+                                name="questioncircleo"
+                                type="antdesign"
+                                color="green"
+                            />
+                        </Tooltip>
                     }
                 />
 
@@ -105,17 +124,17 @@ export default class Validacao extends React.Component{
                     keyboardType="numeric"
                     inputContainerStyle={Styles.input}
                     labelStyle={{color: "green"}}
-                    onChangeText={(value) => this.handleDate(value)}
-                    value={this.state.data}
+                    onChangeText={(value) => this.handleDtnasc(value)}
+                    value={this.state.dtnasc}
                     maxLength={10}
-                    placeholder="Ex: 15/08/1998"
+                    placeholder="Ex: dd/mm/aaaa"
                 />
 
                 <Button 
                     title={"Próximo"}
                     name="arrowright"
                     type="antdesign"
-                    onPress={() => this.checkEmpty()}
+                    onPress={() => this.submitForm()}
                 />
 
             </View>
